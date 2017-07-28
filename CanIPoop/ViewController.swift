f8bb8c8e85e104013c2dee4e7305743775d5b607
Steven Berard <Steven.Berard@EventFarm.com>
2017-07-27 18:54:30 -0700

    Able to attach to database. Starting to build views.

--- CanIPoop/ViewController.swift
+++ CanIPoop/ViewController.swift
@@ -7,19 +7,32 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
 class ViewController: UIViewController {
+
+    var officeRef: DatabaseReference!
+
+    @IBOutlet weak var upperBathroomView: UIView!
+    @IBOutlet weak var lowerBathroomView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        officeRef = Database.database().reference().child("office1")
+
+        print("\nofficeRef: \(officeRef)\n")
+
+        officeRef.observe(.childChanged) { (snapshot: DataSnapshot) in
+            print("key: \(snapshot.key), value: \(snapshot.value)")
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
